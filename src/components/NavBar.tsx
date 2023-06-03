9e5c73a4355a92cb032d72c76721c1968e44cdc1
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-06-03 22:51:20 +0100

    install tailwind, prettier and hamburger-react

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="">
+        Creative<span>art</span>
+      </div>
+      <ul className="list-none">
+        <li>Marketplace</li>
+        <li>Artists</li>
+        <li>Community</li>
+        <li>Collections</li>
+      </ul>
+      <button>Contact</button>
+    </nav>
+  );
+};
+
+export default NavBar;
