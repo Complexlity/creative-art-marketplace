d433cb017fef1854c933b10bb5f134afc8abbac1
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-06-07 15:24:44 +0100

    disable eslint typescript rule

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any,  @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-explicit-any,  @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument */
 
 import { type NextPage } from "next";
 import Head from "next/head";
