f558179ea3417954f3e7259467c8d1723627f60c
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-06-07 18:53:55 +0100

    add mobile nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,14 +3,19 @@ import { ConnectWallet } from "@thirdweb-dev/react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from '../../public/logo.png'
+import { useState } from "react";
 
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const toggledClass = isOpen ? "" : "open"
   return (
     <nav className="flex items-center justify-between text-white">
-      <Link href="/" className="text-3xl flex items-center gap-1">
-        <Image src={logo} className="w-6 h-6" alt="Creative logo" />
-        <p className="logo-text">Creative<span className="text-primary">art</span></p>
+      <Link href="/" className="flex items-center gap-1 text-3xl">
+        <Image src={logo} className="h-6 w-6" alt="Creative logo" />
+        <p className="logo-text">
+          Creative<span className="text-primary">art</span>
+        </p>
       </Link>
       <ul className="hidden list-none items-center gap-8 lg:flex">
         <li className="border-b-2 border-primary">Marketplace</li>
@@ -18,67 +23,46 @@ const NavBar = () => {
         <li>Community</li>
         <li>Collections</li>
       </ul>
-    <div className="hidden lg:block">
-      <ConnectWallet
-      className=""
-      theme="light"
-      dropdownPosition={{
-        align: "center",
-        side: "bottom",
+      <div className="hidden lg:block">
+        <ConnectWallet
+          className=""
+          theme="light"
+          dropdownPosition={{
+            align: "center",
+            side: "bottom",
           }}
-
-      />
-
+        />
       </div>
       <div className="lg:hidden">
-        <Hamburger />
+        <Hamburger toggled={isOpen} toggle={setIsOpen} />
+      </div>
+      <div className={`dropdown_menu absolute right-8 top-[60px] z-10 w-[300px] overflow-hidden rounded-lg bg-gray-400/50 backdrop-blur-sm ${toggledClass} py-2 transition-[height] duration-[.2s] `}>
+        <li className="flex items-center justify-center py-2">
+          <a href="">Home</a>
+        </li>
+        <li className="flex items-center justify-center py-2 ">
+          <a href="">About</a>
+        </li>
+        <li className="flex items-center justify-center   py-2">
+          <a href="">Services</a>
+        </li>
+        <li className="flex items-center justify-center  py-2 ">
+          <a href="">Contact</a>
+        </li>
+        <hr className="mx-auto w-4/5" />
+        <div className="mt-4 text-center py-2 ">
+          <ConnectWallet
+            className=""
+            theme="light"
+            dropdownPosition={{
+              align: "center",
+              side: "bottom",
+            }}
+          />
+        </div>
       </div>
     </nav>
   );
-
-  // return (
-  //   <div className="navbar">
-  //     <div className="navbar-start">
-  //       <details className="dropdown">
-  //         <summary tabIndex={0} className="btn-ghost btn md:hidden">
-  //           <Hamburger />
-  //         </summary>
-  //         <ul
-  //           tabIndex={0}
-  //           className="dropdown-content menu rounded-box menu-lg mt-3 w-96 bg-base-100 p-2 shadow"
-  //         >
-  //           <li>
-  //             <a>Item 1</a>
-  //           </li>
-  //           <li>
-  //             <a>Item 2</a>
-  //           </li>
-  //           <li>
-  //             <a>Item 3</a>
-  //           </li>
-  //         </ul>
-  //       </details>
-  //       <a className="btn-ghost btn text-xl normal-case">DaisyUi</a>
-  //     </div>
-  //     <div className="navbar-center hidden md:flex">
-  //       <ul className="hidden list-none items-center gap-8 md:flex">
-  //          <li className="border-b-2 border-primary">Marketplace</li>
-  //          <li>Artists</li>
-  //          <li>Community</li>
-  //         <li>Collections</li>
-  //       </ul>
-  //     </div>
-  //     <div className="navbar-end">
-  //       <ConnectWallet
-  //         theme="light"
-  //         dropdownPosition={{
-  //           align: "center",
-  //           side: "bottom",
-  //         }}
-  //       />
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default NavBar;
