462be09e4b6adfa92d85201c63c3f6bd78d6ce29
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-06-15 00:41:24 +0100

    fix related items

--- src/pages/nfts/[id].tsx
+++ src/pages/nfts/[id].tsx
@@ -5,12 +5,10 @@ import Footer from "~/components/Universal/Footer";
 import { NFT } from "~/data/nfts";
 import { people, People } from "~/data/people";
 import { useNftsDataContext } from "~/contexts/NftsDataContext";
-// import RelatedItems  from "../../components/Nfts/RelatedItems";
+import RelatedItems  from "../..//components/Nfts/RelatedItems";
 import NftDetails from "~/components/Nfts/NftDetails";
 import { pickRandomItems } from "~/utils/randoms";
 import Head from "next/head";
-// import { NFT } from "~/data/nfts";
-import Card from "~/components/Universal/Card";
 
 function NFTItem() {
   const router = useRouter();
@@ -45,30 +43,5 @@ function NFTItem() {
     </>
   );
 }
-
-
-function RelatedItems({
-  relatedItems,
-}: {
-  relatedItems: NFT[];
-}) {
-  return (
-    <section className="related-items grid gap-12 text-center">
-      <h2 className="relative  text-3xl tracking-wide md:text-4xl">
-        Related Items
-        <span className="absolute bottom-[-.3rem] right-[50%] h-[.15rem] w-[20%] max-w-[180px] translate-x-[50%] bg-primary"></span>
-      </h2>
-      <div
-        suppressHydrationWarning={true}
-        className="related-cards grid gap-6 md:grid-cols-2 lg:grid-cols-3"
-      >
-        {relatedItems.map((item) => {
-          return <Card key={item.id} item={item} />;
-        })}
-      </div>
-    </section>
-  );
-}
-
 
 export default NFTItem;
