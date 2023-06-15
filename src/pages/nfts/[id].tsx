9cd8916aca08b3b5f57e5a4aa13c8e30b4ac1081
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-06-15 23:45:23 +0100

    fix: nft details page fail on first load

--- src/pages/nfts/[id].tsx
+++ src/pages/nfts/[id].tsx
@@ -7,16 +7,36 @@ import { people, People } from "~/data/people";
 import { useNftsDataContext } from "~/contexts/NftsDataContext";
 import RelatedItems  from "~/components/nfts/RelatedItems";
 import NftDetails from "~/components/nfts/NftDetails";
-import { pickRandomItems } from "~/utils/randoms";
+import { generateRandomNFTPrice, generateRandomTimeDifference, pickRandomItems } from "~/utils/randoms";
 import Head from "next/head";
+import { partialNFTsData } from "~/data/nfts";
 
-function NFTItem() {
-  const router = useRouter();
-  const nftsData = useNftsDataContext().nftsData;
-  const id = router.query.id;
+const nftsData: NFT[] = partialNFTsData.map((item) => {
+  item.endTime = generateRandomTimeDifference();
+  item.price = generateRandomNFTPrice();
+  return item as NFT;
+});
+
+
+export const getStaticPaths = () => {
+  const paths = nftsData.map((item) => ({
+    params: {id: item.id}
+  }))
+  return {paths, fallback: true}
+}
+
+export function getStaticProps({ params }: { params: any }) {
+  const nftData = nftsData.find(item => item.id === params.id)
+
+  return { props: { nftData } }
+}
+// const router = useRouter();
+// const nftsData = useNftsDataContext().nftsData;
+// const nftData = nftsData.find((item) => id === item.id) as NFT;
+// const id = router.query.id;
+function NFTItem({ nftData}: { nftData: NFT}) {
   const randomPeople = pickRandomItems(people, 4) as People[];
-  const nftData = nftsData.find((item) => id === item.id) as NFT;
-  let relatedItems = nftsData.filter((item) => id !== item.id);
+  let relatedItems = nftsData.filter((item) => nftData.id !== item.id);
   relatedItems = pickRandomItems(relatedItems, 6) as NFT[];
 
   return (
