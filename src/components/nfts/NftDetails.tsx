cf7b5c25793a09e690acdc430bc0283c62f64b10
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-10-02 23:25:05 +0100

    feat: likes fetching an updating

--- src/components/nfts/NftDetails.tsx
+++ src/components/nfts/NftDetails.tsx
@@ -9,7 +9,11 @@ import useNftBids from "~/hooks/useNftBids";
 import type { Nft, WithUser } from "~/utils/types";
 import HistoryBids from "./HistoryBids";
 import Modals from "./Modals";
-
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { useAuth } from "@clerk/nextjs";
+import useSupabase from "~/hooks/useSupabaseWithAuth";
+import { supabaseWithoutClient as supabase, supabaseWithClient as supabaseClient } from "supabase";
+import { useMemo } from "react";
 
 export default function NftDetails({
   nftData,
@@ -20,13 +24,47 @@ export default function NftDetails({
 }) {
 
   const pathname = usePathname()
+  const { userId, getToken } = useAuth()
+  // const supabase = useSupabase()
+  const queryClient = new QueryClient()
+
 const currentPathname = pathname.split('/').pop()!
   const { data: bids, isLoading: isFetchingBids } = useNftBids({currentPathname})
 
   if (!nftData)
     return <div>Not Found</div>;
 
+  const { data: likes } = useQuery({
+    queryKey: ['likes', nftData.slug],
+    queryFn: async () => {
+      const { data: likes, error } = await supabase.from('nft_likes').select('*').eq('nft_slug', nftData.slug)
+      return likes
+    }
+  })
 
+  console.log(likes)
+  const likedByMe = !!likes?.find(obj => obj.user_id === userId)
+
+  const { mutate: likePost } = useMutation({
+    mutationKey: ['like-post'],
+    mutationFn: async () => {
+      const supabaseAccessToken = await getToken({
+        template: "supabase",
+      });
+      const supabase = await supabaseClient(supabaseAccessToken);
+      const { data, error } = await supabase.from('nft_likes').insert({
+        user_id: userId,
+        nft_slug: nftData.slug
+      }).select()
+      if(error) console.log(error)
+      return data
+    },
+    onSuccess: (data) => {
+      console.log("I was a success")
+      console.log(data)
+      queryClient.invalidateQueries(['likes', nftData.slug])
+    }
+  })
 
 
   return (
@@ -44,7 +82,7 @@ const currentPathname = pathname.split('/').pop()!
         <small className="auction-time">
           {" "}
           <span className="text-gray-400">Auctions end in</span>{" "}
-          {/* @ts-ignore time function not created */}
+          {/* @ts-ignore time function not created yet */}
           <CountDownComponent timeDifference={nftData.endTime} />
         </small>
 
@@ -61,8 +99,12 @@ const currentPathname = pathname.split('/').pop()!
           <div className="flex items-center gap-1 rounded-sm bg-slate-500 px-4 py-[.1rem] text-gray-300 shadow-md shadow-gray-700 duration-300 hover:-translate-y-1">
             <AiFillEye /> {getRandomNumber(1000)}
           </div>
-          <div className="flex items-center gap-1 rounded-sm bg-slate-500 px-4 py-[.1rem] text-gray-300 shadow-md shadow-gray-700 duration-300 hover:-translate-y-1">
-            <AiFillHeart /> {getRandomNumber(100)}
+          <div
+            // @ts-ignore cannot pass handler for ts reasons
+            onClick={likePost}
+            className="flex items-center gap-1 rounded-sm bg-slate-500 px-4 py-[.1rem] text-gray-300 shadow-md shadow-gray-700 duration-300 hover:-translate-y-1 hover:cursor-pointer"
+          >
+            <AiFillHeart fill={likedByMe ? `#be123c` : ""} /> {likes?.length}
           </div>
         </div>
         <p className="item-description text-gray-400"> {nftData.description}</p>
@@ -81,12 +123,12 @@ const currentPathname = pathname.split('/').pop()!
                 alt=""
               />
             </div>
-            <p className="font-semibold tracking-widest">{nftData.users.username}</p>
+            <p className="font-semibold tracking-widest">
+              {nftData.users.username}
+            </p>
           </div>
         </div>
-        {isFetchingBids
-          ? "Fetching bids...."
-          : <HistoryBids bids={bids} />}
+        {isFetchingBids ? "Fetching bids...." : <HistoryBids bids={bids} />}
         <Modals nftData={nftData} />
       </div>
     </section>
