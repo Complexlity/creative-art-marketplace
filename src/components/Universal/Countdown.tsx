22d14dd00f7dbd14ea6d7adbd7c21c3b0b596d28
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-01-14 08:05:47 +0100

    refactor: countdown timer

--- src/components/Universal/Countdown.tsx
+++ src/components/Universal/Countdown.tsx
@@ -1,27 +1,66 @@
-import Countdown, { zeroPad} from 'react-countdown'
-
+import Countdown, { zeroPad } from "react-countdown";
 
 type Renderer = {
-  hours: number,
-  minutes: number,
-  seconds: number,
-  completed: boolean
-}
+  hours: number;
+  minutes: number;
+  seconds: number;
+  completed: boolean;
+};
 //@ts-ignore
 let renderer = ({ days, hours, minutes, seconds, completed }: Renderer) => {
-  hours += days * 24
+  hours += days * 24;
   const Completionist = () => <span className="text-green-500">NIL</span>;
   if (completed) {
     // Render a completed state
-  return <Completionist />;
-} else {
-  // Render a countdown
-    return <span suppressHydrationWarning={true}>{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}</span>;
-}
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span suppressHydrationWarning={true}>
+        {zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+      </span>
+    );
+  }
 };
 
-export default function CountDownComponent({ timeDifference }: { timeDifference: number }) {
+function convertStringDateToMilleseconds(date: string) {
+  if(!date) return 0
+  const dateObject = new Date(date);
+  return dateObject.setHours(0, 0, 0, 0)};
 
-  return <Countdown date={Date.now() + timeDifference} renderer={renderer} daysInHours/>
+
+
+export default function CountDownComponent({
+  startDate, endDate
+}: {
+    startDate: string,
+  endDate: string
+  })
+
+{
+
+  // console.log("I am here")
+  //     let dateCounted = 0
+  // let start = convertStringDateToMilleseconds(startDate);
+  //     let now = Date.now();
+  // let end = convertStringDateToMilleseconds(endDate);
+
+  //   if (start > now) {
+  //     dateCounted = start;
+
+  //     } else if (end > now) {
+  //     dateCounted = end;
+  // }
+  // else { dateCounted = Date.now() }
+
+
+
+  // console.log(`${dateCounted} was set`)
+  return (
+    <Countdown
+      date={Date.now()  + 1000}
+      renderer={renderer}
+      daysInHours
+    />
+  );
 }
-
